22044f48d54296ea04492ac2b324135ebb583436
Pileks <pilex13@gmail.com>
2023-10-12 09:49:00 +0200

    add requireSign to signerAddress function, for wallet needs

--- app/functions/general/signerAddress.py
+++ app/functions/general/signerAddress.py
@@ -9,6 +9,7 @@ function: dict[str, Any] = {
         "parameters": {"type": "object", "properties": {}, "required": []},
     },
     "description": "Get the address of the connected signer. It can be used to get ethereum address of the user.",
+    "requireSign": True, # Should be requireWallet, but we're keeping all wallet-related stuff as `requireSign` at the moment
     "invocation": {
         "uri": "wrap://wrapscan.io/polywrap/ethers@1.1.1",
         "method": "getSignerAddress",
